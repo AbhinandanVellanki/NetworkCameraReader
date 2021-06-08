a6af40f664f5cf6b5ef1a1e90f97b967a22749fc
AbhinandanVellanki <abhinandan.vellanki@gmail.com>
2021-06-08 10:43:35 +0800

    Added Debug messages and Gstreamer Pipeline options

--- read.py
+++ read.py
@@ -5,7 +5,10 @@ import numpy as np
 class VideoCapture:
 
     def __init__(self,username,password,ip_address):
+        pipeline = "rtspsrc location=rtsp://admin:Aiacademy2021@192.168.50.7:554 latency=0 ! rtph265depay ! queue ! h265parse ! omxh265dec ! videoconvert ! appsink"
+        gst = "rtspsrc location=rtsp://admin:Aiacademy2021@192.168.50.7:554/test latency=0 ! rtph265depay ! h265parse ! omxh265dec ! videoconvert ! appsink"
         self.cap = cv2.VideoCapture(f"rtsp://{username}:{password}@{ip_address}:554/cam/realmonitor?channel=1@subtype=1")
+        #self.cap = cv2.VideoCapture(pipeline)
         self.q = queue.Queue()
         t = threading.Thread(target=self._reader)
         t.daemon = True
@@ -16,6 +19,7 @@ class VideoCapture:
         while True:
             ret, frame = self.cap.read()
             if not ret:
+                print("Reading Error!")
                 break
             if not self.q.empty():
                 try:
